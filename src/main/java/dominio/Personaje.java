f7497409524bb1ecd5610018b50e195a899f6c87
jorge <jamc4626@gmail.com>
2017-09-30 20:19:02 -0300

    Revert "agregados metodos para aumentar directamente el ataque y la defensa"
    This reverts commit 63ac5cf14d337c52577437b6b67aca7026360f77.

--- src/main/java/dominio/Personaje.java
+++ src/main/java/dominio/Personaje.java
@@ -127,7 +127,10 @@ public abstract class Personaje extends MadreDeTodo implements Peleable, Seriali
 	 * Posicion inicial del personaje en Y.
 	 */
 	private static final int POSYI = 0;
-
+	/**
+	 * Ataque del personaje.
+	 */
+	private int ataque;
 	/**
 	 * Magia del personaje.
 	 */
